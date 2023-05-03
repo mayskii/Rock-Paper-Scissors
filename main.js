9314b072f0219da221996411fb382f78f7548a2c
Mayskii <mayzakharova@gmail.com>
2023-05-02 21:00:53 -0500

    function Game updated-2

--- main.js
+++ main.js
@@ -4,40 +4,49 @@ let computerSelection;
 let playerSelection;
 
 function game() {
-    
-    for (let i = 0; i < 5; i++) {   
-        
-        function getComputerChoice() {
-            const choice = ["rock", "paper", "scissors"];
-            let random = Math.floor(Math.random()* choice.length);
-            // console.log(choice[random]);
-            return choice[random];
-        }
-       
-        function playRound(playerSelection, computerSelection) {
-            playerSelection = window.prompt("Inter your chose - rock, paper or scissors"); 
-            computerSelection = getComputerChoice();
+  for (let i = 0; i < 5; i++) {
+    function getComputerChoice() {
+      const choice = ["rock", "paper", "scissors"];
+      let random = Math.floor(Math.random() * choice.length);
+      return choice[random];
+    }
 
-            if ((playerSelection.toLowerCase() === "rock" && computerSelection === "paper")||(playerSelection.toLowerCase() === "paper" && computerSelection === "scissors")||(playerSelection.toLowerCase() === "scissors" && computerSelection === "rock")) {
-                console.log (`You Lose! ${computerSelection} beats ${playerSelection}`);
-                countComputerWin++;
+    function playRound(playerSelection, computerSelection) {
+      playerSelection = window.prompt(
+        "Inter your chose - rock, paper or scissors"
+      );
+      computerSelection = getComputerChoice();
 
-            } else if (playerSelection.toLowerCase() === computerSelection) {
-                console.log(`Nobody Win!`);
-          
-            } else {
-                console.log(`You Win! ${playerSelection} beats ${computerSelection}`)
-                countPlayerWin++;}
+      if (
+        (playerSelection.toLowerCase() === "rock" &&
+          computerSelection === "paper") ||
+        (playerSelection.toLowerCase() === "paper" &&
+          computerSelection === "scissors") ||
+        (playerSelection.toLowerCase() === "scissors" &&
+          computerSelection === "rock")
+      ) {
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+        countComputerWin++;
+      } else if (playerSelection.toLowerCase() === computerSelection) {
+        console.log(`Nobody Win!`);
+      } else {
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+        countPlayerWin++;
+      }
 
-            // console.log(`Computer ${countComputerWin}: Player ${countPlayerWin}`);
-            return (countComputerWin, countPlayerWin);
-        }   
-        playRound();
-    } 
-    if (countComputerWin > countPlayerWin) {
-            console.log(`Computer Win! ${countComputerWin}:${countPlayerWin}`)
-    } else if ((countComputerWin < countPlayerWin)) {
-        console.log(`Player Win! ${countPlayerWin}:${countComputerWin}`)
-    } else {console.log(`Nobody Win! Same numbers! ${countPlayerWin}:${countComputerWin}`)}
+      // console.log(`Computer ${countComputerWin}: Player ${countPlayerWin}`);
+      return countComputerWin, countPlayerWin;
+    }
+    playRound();
+  }
+  if (countComputerWin > countPlayerWin) {
+    console.log(`Computer Win! ${countComputerWin}:${countPlayerWin}`);
+  } else if (countComputerWin < countPlayerWin) {
+    console.log(`Player Win! ${countPlayerWin}:${countComputerWin}`);
+  } else {
+    console.log(
+      `Nobody Win! Same numbers! ${countPlayerWin}:${countComputerWin}`
+    );
+  }
 }
 game();
